49fa2395f750074af7bcd7f72bcd78ce24034991
mrX0xo <farizkito18@gmail.com>
2024-12-16 19:29:56 +0700

    Latihan 2 hari 1

---
+++ konstanta.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+const pi = 3.14
+
+func main() {
+	fmt.Println("Belajar konstanta")
+	fmt.Println(pi)
+}
---
+++ tipeData.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar Tipe Data di Golang")
+	var nama string = "Fariz"
+	var umur = 23
+	hobi := "Bermain Game"
+	var boolstate bool = true
+	var tinggi float32 = 177.5
+	var a, b, c = 1, 2, 3
+
+	fmt.Println(nama)
+	fmt.Println(umur)
+	fmt.Println(hobi)
+	fmt.Println(boolstate)
+	fmt.Println(tinggi)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
